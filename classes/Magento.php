0ac5817d67882c706a6cc9b77528b456eb2213b7
arioch1984 <arioch1984@gmail.com>
2014-11-13 12:28:01 +0100

    debugging api call result sometime would not display anything - caching WP side problem

--- classes/Magento.php
+++ classes/Magento.php
@@ -54,6 +54,8 @@ class Magento {
 				$content .= $CC->getCache();
 				$runApiCalls = false;
 			}catch(Exception $e){
+				error_log('Cache Error:');
+				error_log(print_r($e,true));
 				$runApiCalls = true;
 			}
 		}
